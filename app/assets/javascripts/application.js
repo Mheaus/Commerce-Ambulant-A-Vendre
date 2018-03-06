c38a8ce5924c45879d9d0ab7701a8f2262bdd7eb
mathieu audebert <mathieu.adbrt@gmail.com>
2018-03-06 20:28:37 +0100

    add hyperloop to project

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require react_ujs
 //= require turbolinks
+//
 //= require_tree .
+//
+//= require hyperloop-loader
